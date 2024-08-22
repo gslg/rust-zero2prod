affcbbaeebb7954ed62e996b1347f1a97d338071
lg <sclg1992@163.com>
2024-08-22 20:28:07 +0800

    add tcp listener

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+use std::net::TcpListener;
+
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
-pub fn run(address: &str) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(address)?
+        .listen(listener)?
         .run();
     // No .await here!
     Ok(server)
