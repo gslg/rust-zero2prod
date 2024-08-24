db0d8c3e93d726cea6a65d16af8778940c770470
lg <sclg1992@163.com>
2024-08-24 11:57:35 +0800

    parse http request to struct model

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,38 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // No .await here!
     Ok(server)
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+// We were returning `impl Responder` at the very beginning.
+// We are now spelling out the type explicitly given that we have
+// become more familiar with `actix-web`.
+// There is no performance difference! Just a stylistic choice :)
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+//  always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use reqwest::Client;
+
 // 刚开始我开了代理，一直测试不成功，状态一直502...。
 // 关闭代理后就测试通过了.
 #[tokio::test]
@@ -39,4 +41,55 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{app_address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    let url = format!("{}/subscriptions", app_address);
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&url)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
