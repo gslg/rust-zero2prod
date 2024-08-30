194c9c387680d810233207169c87fff7224fc90c
lg <sclg1992@163.com>
2024-08-30 21:23:26 +0800

    refactoring application config

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,10 @@ async fn main() -> Result<(), std::io::Error> {
             .expect("Failed to connect to Postgres.");
 
     // We have removed the hard-coded `8000` - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind port");
 
     run(listener, connection_pool)?.await?;
